3b7d5ffa673e9f60a1d2b3978f81ef50eab7e0b2
rhg10fl <rocio.hdz.gtz@gmail.com>
2023-12-07 22:29:59 -0600

    nuevos archivos

---
+++ JavaScript/script.js
@@ -0,0 +1,61 @@
+//cometario de una sola linea
+
+/* comentario
+multilínea*/
+
+
+//alert ("Hola Mundo");
+
+
+// console.log ("Este mensaje es para la consola");
+
+
+/*var nombre = prompt("Ingresa tu nombre");
+alert ("Hola, "+nombre + "Bienvenido a la CH35")*/
+
+
+/* var numero1 = parseInt (prompt("Ingresa el primer número:"));
+var numero2 = parseInt (prompt("Ingresa el segundo número:"))
+
+console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicación:" + (numero1 * numero2));
+console.log("division:" + (numero1 / numero2));
+*/
+
+var edad = prompt("Bienvenido al bar. Ingresa tu edad");
+
+if (edad <18)  {
+    alert("Bebecito, No puedes pasar");
+}
+if (edad >= 18 && edad <=65)  {
+    alert("Eres mayor de 18 años y lo suficientemente cool para pasar, Adelante");
+}
+if (edad >65)  {
+    alert("Demasiado viejo hermano, No puedes pasar");
+}
+
+/*var edad = prompt("Bienvenido al bar. Ingresa tu edad");
+
+if (edad <18)  {
+    alert("Bebecito, No puedes pasar");
+}
+if (edad >= 18 && edad <=65)  {
+    alert("Eres mayor de 18 años y lo suficientemente cool para pasar, Adelante");
+}
+if (edad >65)  {
+    alert("Demasiado viejo hermano, No puedes pasar");
+} */
+
+    /*var nombre = prompt("Ingresa tu nombre");
+var edad = parseInt(prompt("Cual es tu edad"));
+
+if (edad>40) {
+    alert("HOLA, " + nombre + " Ya tas viejo");
+    } else if(edad>30) {
+    alert("HOLA, " + nombre + " Eres un chavo ruco >.<");
+  } else if(edad>=18) {
+    alert("HOLA, " + nombre + " ya eres adulto 3:)");
+  }else if(edad<18){
+    alert("HOLA, " + nombre + " eres un chavo");
+  }*/
